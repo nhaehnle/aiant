8623112b847cd1eb143dddaa25f2d84f77b7bc67
Nicolai Hähnle <nhaehnle@gmail.com>
2011-12-08 21:10:53 +0100

    Add extracttimings.py
    Signed-off-by: Nicolai Hähnle <nhaehnle@gmail.com>

---
+++ extracttimings.py
@@ -0,0 +1,42 @@
+# Extract timing information from a debug.txt
+
+import re
+import sys
+
+turn=None
+v={}
+v["moves"]=None
+v["evals"]=None
+v["time"]=None
+
+turnre = re.compile("turn (\d+):")
+timere = re.compile("time taken in turn (\d+): ([0-9.]+)ms")
+moveevalre = re.compile("Total number of generated moves: (\d+), evaluated pairs: (\d+)")
+
+print ' '*3, ' '.join(['{0:10}'.format(key) for key in v.iterkeys()])
+
+for line in sys.stdin:
+	m = turnre.match(line)
+	if m:
+		turn=int(m.groups()[0])
+		for key in v.iterkeys():
+			v[key] = None
+	else:
+		m = timere.match(line)
+		if m:
+			v["time"] = m.groups()[1]
+		else:
+			m = moveevalre.match(line)
+			if m:
+				v["moves"] = m.groups()[0]
+				v["evals"] = m.groups()[1]
+
+	if turn != None:
+		for key in v.iterkeys():
+			if v[key] == None:
+				break
+		else:
+			print '{0:0<3}'.format(turn), ' '.join('{0:>10}'.format(v[key]) for key in v.iterkeys())
+			for key in v.iterkeys():
+				v[key] = None
+				turn = None
